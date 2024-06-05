73a6a0e133877bed9babc037c2765c52b072a25c
Risler Jeremie <jeremie.risler@student-cs.fr>
2024-06-05 16:00:01 +0200

    rnaed

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -44,10 +44,6 @@ function Home() {
     <div className="App">
       <header className="App-header">
         <h1> CACAFLIX </h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to caca.
-        </p>
         <input
           type="text"
           value={movieName}
