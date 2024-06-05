2153669190c770ebc10a3d6ffaf7fe1fbb3fdb97
noe.madranges <noe.madranges@student-cs.fr>
2024-06-05 15:39:37 +0200

    caca

--- frontend/src/components/Movie/movie.jsx
+++ frontend/src/components/Movie/movie.jsx
@@ -5,18 +5,19 @@ const Movie = ({ movie }) => {
   const { title, release_date, poster_path, genre_ids } = movie;
   const imageUrl = `https://image.tmdb.org/t/p/w500${poster_path}`;
 
-  const handleMovieClick = () => {
-    const wikiUrl = `https://en.wikipedia.org/wiki/${encodeURIComponent(
-      title
-    )}`;
-    window.open(wikiUrl, '_blank');
-  };
-
   return (
     <div className="movie">
       <img src={imageUrl} alt={title} />
       <h2>{title}</h2>
       <p>Release Date: {release_date}</p>
+
+      <div className="genres">
+        {genre_ids.map((genre, genreIndex) => (
+          <span key={genreIndex} className="genre-box">
+            {genre}
+          </span>
+        ))}
+      </div>
     </div>
   );
 };
