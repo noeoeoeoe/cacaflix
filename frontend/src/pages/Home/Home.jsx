17750fcec074a91f6625dbc3d7bc6156edc3a181
noe.madranges <noe.madranges@student-cs.fr>
2024-06-05 15:04:50 +0200

    caca

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -5,9 +5,10 @@ function Home() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1> CACAFLIX </h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.jsx</code> and save to reload.
+          Edit <code>src/App.jsx</code> and save to caca.
         </p>
         <a
           className="App-link"
